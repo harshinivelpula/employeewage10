4aded280adf85bb465c0abfc3dbb6bf7b0bcd6db
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-23 17:58:20 +0530

    adding program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace companywage213
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("welcome to employee wage computation");
+            attendance.checkattendance();
+        }
+    }
+}
