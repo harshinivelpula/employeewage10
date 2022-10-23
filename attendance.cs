7602f74b9049f3a74885032a7b61384e636e3662
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-23 17:59:55 +0530

    check attendance

---
+++ attendance.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employeewageday10
+{
+    internal class attendance
+    {
+        public static void checkattendance()
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empcheck = random.Next(0, 2);
+            if (empcheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("employee is present");
+            }
+            else
+            {
+                Console.WriteLine("employee is absent");
+            }
+        }
+    }
+}
+
